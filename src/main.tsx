4558b297f9fb20da8d403a778161cd85f8c30932
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-24 01:17:21 -0300

    feat: config web vitals, config resize observer

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
 import App from "@/App";
 import "@/global";
 import routes from "@/routes";
+import { IframeMessageProxy } from "iframe-message-proxy";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ResizeObserver } from "resize-observer";
+import TrackingEvents from "./constants/tracking-events";
+import reportWebVitals from "./reportWebVitals";
+import { track } from "./services/analytics-service";
+import { setHeight } from "./services/common-service";
 
 const container = document.getElementById("root") as HTMLElement;
+
+// Resize app in the Portal
+const observer = new ResizeObserver(() => {
+  setHeight(container.scrollHeight);
+});
+observer.observe(container);
+
+// handle communication through browser post messages
+IframeMessageProxy.listen();
 
 const root = createRoot(container);
 
@@ -18,8 +33,16 @@ const router = createBrowserRouter([
   },
 ]);
 
+// segment tracking - extension opened
+track(TrackingEvents.EXTENSION_OPENED);
+
 root.render(
   <StrictMode>
     <RouterProvider router={router} fallbackElement={<div>loading...</div>} />
   </StrictMode>,
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
