d194542fe7e1aa2735d3cdc372154a52a384351a
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-10-24 01:06:26 -0300

    test: add tests base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 import tsPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+    },
+    setupFiles: "./tests/setup.ts",
+  },
   server: {
     https: true,
     port: 5173,
